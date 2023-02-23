170924f970f0e740d24023b6efd9413ca6c21ba1
katsup <brett_7y7@yahoo.ca>
2023-02-23 16:16:41 +0900

    Hide header login button when on login page

--- components/layout/MainHeader.js
+++ components/layout/MainHeader.js
@@ -8,7 +8,7 @@ const MainHeader = () => {
 	const router = useRouter();
 	const isAuth = useSelector((state) => state.authReducer.isAuth);
 	const username = useSelector((state) => state.authReducer.username);
-
+ console.log('router.query.decadeId: ', router.query.decadeId);
 	return (
 		<header className={classes.header}>
 			<nav className={classes.nav}>
@@ -19,45 +19,45 @@ const MainHeader = () => {
 						</Link>
 					</li>
 					<li className={classes.auth}>
-						<BaseButton
+					{router.pathname !== '/auth' && <BaseButton
 							onClick={() => router.push('/auth')}
-							className={router.pathname === '/auth' ? 'active' : ''}>
+              >
 							{isAuth ? `Logout - ${username}` : `Login`}
-						</BaseButton>
+						</BaseButton>}
 					</li>
 				{ isAuth && <li className={classes.auth}>
 						<BaseButton
 							onClick={() => router.push('/userPage')}
 							className={router.pathname === '/auth' ? 'active' : ''}>
-							UserAdmin
+							Editor
 						</BaseButton>
 					</li>}
 					<div className={classes.decades}>
 						<li>
 							<Link
-								href='/1960s'
-								className={router.pathname === '/1960s' ? 'active' : ''}>
+								href='/decades/1960s'
+								className={router.query.decadeId === '1960s' ? 'active' : ''}>
 								1960s
 							</Link>
 						</li>
 						<li>
 							<Link
-								href='/1970s'
-								className={router.pathname === '/1970s' ? 'active' : ''}>
+								href='/decades/1970s'
+								className={router.query.decadeId === '1970s' ? 'active' : ''}>
 								1970s
 							</Link>
 						</li>
 						<li>
 							<Link
-								href='/1980s'
-								className={router.pathname === '/1980s' ? 'active' : ''}>
+								href='/decades/1980s'
+								className={router.query.decadeId === '1980s' ? 'active' : ''}>
 								1980s
 							</Link>
 						</li>
 						<li>
 							<Link
-								href='/1990s'
-								className={router.pathname === '/1990s' ? 'active' : ''}>
+								href='/decades/1990s'
+								className={router.query.decadeId === '1990s' ? 'active' : ''}>
 								1990s
 							</Link>
 						</li>
